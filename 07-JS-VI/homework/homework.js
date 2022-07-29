9ef1c77124d3b54d5131aff00eb77ba1b36189fc
Gonzalo Watson <gonzawatson1116@gmail.com>
2022-07-29 13:18:03 -0300

    Tarea 07JS Resultado

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -45,10 +45,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  var nuevoArray = array.map(function(elemento){
+  var arrayNuevo = array.map(function(elemento){
     return cb(elemento) 
   });
-return nuevoArray;
+return arrayNuevo;
 }
 
 function filter(array) {
